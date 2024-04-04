e016f25de6fa074952b84a181feef930c4373fff
a100440 <160506851+a100440@users.noreply.github.com>
2024-04-04 10:39:09 +0100

    Mudanças BD

--- BD.pl
+++ BD.pl
@@ -1,6 +1,39 @@
 :- dynamic doenca/2.
 :- dynamic tratamento/4.
 
+%doenca(nome, sintomas)
+%medicamento(nome, tratamento, tipomedicamento, medicamento, idadeTratamento, pesotratamento, dose, VezesAoDIA condicaoespecial)
+
+doenca(pneumonia, sintomas(febre, arrepios de frio, tosse com expetoracao, dificuldade respiratoria, dor toracica, dor de cabeca, dor muscular))   
+
+medicamento(pneumonia, administracao de analgesicos e anti inflamatorios, Xarope-suspensao oral, Paracetamol ben-u-ron , +3meses, 
+null, Consultar bula, consultar bula, null)
+
+medicamento(pneumonia,administracao de antibioticos, Amoxicilina,comprimido, null, +40kg, 750mg-3g, 2-3, pessoas com hipersensibilidade a antibioticos beta-lactamicos)
+
+medicamento(pneumonia, administracao de antibioticos, Xarope-suspensao oral,Amoxicilina, +3anos, null, 5ml de 500mg, 8h-8h, 
+pessoas com hipersensibilidade a antibioticos beta-lactamico)
+
+medicamento(pneumonia, administracao de antibioticos, Xarope-suspensao oral,Amoxicilina, null, -6kg, 2.5ml de 250mg, 8h-8h,
+pessoas com hipersensibilidade a antibioticos beta-lactamicos)
+
+medicamento(pneumonia, administracao de analgesicos e anti inflamatorios, comprimido, Paracetamol ben-u-ron , +12, 
+null, 1 a 2 comprimidos de 500mg, 4-6 horas, null)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 doenca(asma, sintomas(pieira, falta de ar, sensacao de aperto no peito, tosse)).
 
 doenca(pneumonia, sintomas(febre, arrepios de frio, tosse com expetoração, dificuldade respiratória, dor tóracica, dor de cabeça, dor muscular)).
