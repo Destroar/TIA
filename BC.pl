bcda0b1844ea98ca5794ab778170d6330335c29f
Destroar <126820622+Destroar@users.noreply.github.com>
2024-04-04 10:48:09 +0100

    Create BC.pl

---
+++ BC.pl
@@ -0,0 +1,8 @@
+:- op( 800, fx, if).
+:- op( 700, xfx, then).
+:- op( 300, xfy, or).
+:- op( 200, xfy, and)
+
+if tosse com expetoração and dificuldade respiratória and (febre or arrepios de frio or dor tóracica or dor de cabeça or dor muscular) then pneumonia
+
+if placas de uma substância cremosa na boca and aparecimento de aftas na língua ou na bochecha and (sensação de algodão dentro da boca or dor ou ardência nas regiões afetadas or camada esbranquiçada na boca) then candidíase oral
