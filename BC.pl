0055b776c2177b60dbb4a23b5eb79d72c95dff1e
Destroar <rodrigojoao2003@hotmail.com>
2024-04-07 12:25:54 +0100

    Mudanças

--- BC.pl
+++ BC.pl
@@ -1,10 +1,28 @@
 :- op( 800, fx, if).
 :- op( 700, xfx, then).
 :- op( 300, xfy, or).
-:- op( 200, xfy, and)
+:- op( 200, xfy, and).
 
 if tosse_com_expetoracao and dificuldade_respiratoria and 
-(febre or arrepios_de_frio or dor_toracica or dor_de_cabeca or dor_muscular) then pneumonia.
+(febre or arrepios_de_frio or dor_toracica or dor_cabeca or dor_muscular) then pneumonia.
 
 if placas_de_uma_substancia_cremosa_na_boca and aparecimento_de_aftas_na_lingua_ou_na_bochecha and 
 (sensacao_de_algodo_dentro_da_boca or dor_ou_ardencia_nas_regioes_afetadas or camada_esbranquicada_na_boca) then candidiase_oral.
+
+if dor_garganta and (febre or dificuldade em engolir or dor_de_ouvidos or mal-estar_geral) then amigdalite.
+
+if dor_ouvido and (dificuldade_em_dormir or choro_superior_ao_normal or dificuldades_de_audicao or febre
+or falta_apetite or dor_cabeca) then otite.
+
+if dor_cabeca and febre and (erupcoes_cutaneas or calafrios or nauseas or diarreia or dificuldade_em_urinar or dores_formigueiros_picadas_pele) then zona.
+
+if ardor_ou_dor_ao_urinar and vontade_urgente_e_frequente_de_urinar and (ocorrencia_de_miccoes_frequentes or urina_com_cheiro_fetido or
+alteracoes_na_cor_da_urina or dificuldade_a_iniciar_a_miccao or eliminacao_de_sangue_na_urina or dor_na_parte_inferior_do_abdomen or
+febre and calafrios or dor_lombar and nauseas and vomitos) then infeccao_urinaria.
+
+if coceira_na_boca and (coceira_na_pele or dor_abdominal or diarreia or enjoos or mal_estar_na_garganta or falta_ar or dificuldade_ao_respirar or rouquidao) then alergiaAlimentar.
+
+if nariz_entupido and espirros constantes and (coceira_nariz_olhos_garganta or nariz vermelho or tosse seca or dificuldade_respirar or olhos vermelhos or dor_cabeca)
+then alergiaRespiratoria.
+
+if tosse and falta_ar and (dor_peito or febre_baixa or fadiga) then bronquite.
--- BD.pl
+++ BD.pl
@@ -14,42 +14,42 @@
 
 % medicamento(nome, tratamento, tipomedicamento, medicamento, idadeTratamento, pesotratamento, prescricao, dose, VezesAoDIA, condicaoespecial)
 
-medicamento(pneumonia, 1, Xarope, ParacetamolBen-u-ron , mais3meses, entre3e6kg, entre0.4e0.8ml, 100mgporml, entre4e6horas, [doencasfigado]).
-medicamento(pneumonia, 1, Xarope, ParacetamolBen-u-ron , mais3meses, entre6e9kg, entre0.8e1.1ml, 100mgporml, entre4e6horas, [doencasfigado]).
-medicamento(pneumonia, 1, Xarope, ParacetamolBen-u-ron , mais3meses, entre9e12kg, entre1.1e1.5ml, 100mgporml, entre4e6horas, [doencasfigado]).
-medicamento(pneumonia, 1, Xarope, ParacetamolBen-u-ron , mais3meses, entre12e15kg, entre1.9e1.5ml, 100mgporml, entre4e6horas, [doencasfigado]).
-medicamento(pneumonia, 1, Xarope, ParacetamolBen-u-ron , mais3meses, entre15e20kg, entre2.5e1.9ml, 100mgporml, entre4e6horas, [doencasfigado]).
-medicamento(pneumonia, 2 , Comprimido , Amoxicilina , null , mais40kg, entre250mga1g, null, 2a3vezes , [hipersensibilidadeAntibioticosBetaLactamicos]).
-medicamento(pneumonia, 2 , Xarope , Amoxicilina , mais3anos, null, 5ml, 500mgpor5ml , 8em8horas, [hipersensibilidadeAntibioticosBetaLactamicos]).
-medicamento(pneumonia, 2 , Xarope , Amoxicilina , menos3anos, null, 5ml, 250mgpor5ml , 8em8horas, [hipersensibilidadeAntibioticosBetaLactamicos]).
-medicamento(pneumonia, 2 , Xarope ,Amoxicilina, null, menos6kg, 2.5ml, 250mgpor5ml, 8em8horas , [hipersensibilidadeAntibioticosBetaLactamicos]).
-medicamento(pneumonia, 1 , ComprimidoEferverescente, ParacetamolBen-u-ron , mais12anos , null, 500mg, null, 4a6horas, [doencasfigado]).
-medicamento(pneumonia, 1 , Comprimido, ParacetamolBen-u-ron , mais12anos , null, 750mg, null, 2a3vezes, [doencasfigado]).
-medicamento(pneumonia, 1 , Comprimido, Brufenon , mais18anos , null, 700mg, null, 6em6horas, [Gravidez, Amamentacao]).
-medicamento(candidiaseOral, 3, Pomada, Clotrimazol, null, null, null, null, 2a3vezesdia , [hipersensibilidadeClotrimazol,Gravidez, Amamentacao]).
-medicamento(candidiaseOral, 3, Pomada, Fluconazol, mais18anos, null, 400mginiciale200depois, null, 1vezdia , [hipersensibilidadefluconazol]).
+medicamento(pneumonia, 1, xarope, paracetamolBen-u-ron , mais3meses, entre3e6kg, entre0.4e0.8ml, 100mgporml, entre4e6horas, [doencasfigado]).
+medicamento(pneumonia, 1, xarope, paracetamolBen-u-ron , mais3meses, entre6e9kg, entre0.8e1.1ml, 100mgporml, entre4e6horas, [doencasfigado]).
+medicamento(pneumonia, 1, xarope, paracetamolBen-u-ron , mais3meses, entre9e12kg, entre1.1e1.5ml, 100mgporml, entre4e6horas, [doencasfigado]).
+medicamento(pneumonia, 1, xarope, paracetamolBen-u-ron , mais3meses, entre12e15kg, entre1.9e1.5ml, 100mgporml, entre4e6horas, [doencasfigado]).
+medicamento(pneumonia, 1, xarope, paracetamolBen-u-ron , mais3meses, entre15e20kg, entre2.5e1.9ml, 100mgporml, entre4e6horas, [doencasfigado]).
+medicamento(pneumonia, 2 , comprimido , amoxicilina , null , mais40kg, entre250mga1g, null, 2a3vezes , [hipersensibilidadeAntibioticosBetaLactamicos]).
+medicamento(pneumonia, 2 , xarope , amoxicilina , mais3anos, null, 5ml, 500mgpor5ml , 8em8horas, [hipersensibilidadeAntibioticosBetaLactamicos]).
+medicamento(pneumonia, 2 , xarope , amoxicilina , menos3anos, null, 5ml, 250mgpor5ml , 8em8horas, [hipersensibilidadeAntibioticosBetaLactamicos]).
+medicamento(pneumonia, 2 , xarope ,amoxicilina, null, menos6kg, 2.5ml, 250mgpor5ml, 8em8horas , [hipersensibilidadeAntibioticosBetaLactamicos]).
+medicamento(pneumonia, 1 , comprimidoEferverescente, paracetamolBen-u-ron , mais12anos , null, 500mg, null, 4a6horas, [doencasfigado]).
+medicamento(pneumonia, 1 , comprimido, paracetamolBen-u-ron , mais12anos , null, 750mg, null, 2a3vezes, [doencasfigado]).
+medicamento(pneumonia, 1 , comprimido, brufenon , mais18anos , null, 700mg, null, 6em6horas, [gravidez, amamentacao]).
+medicamento(candidiaseOral, 3, pomada, clotrimazol, null, null, null, null, 2a3vezesdia , [hipersensibilidadeclotrimazol,gravidez, amamentacao]).
+medicamento(candidiaseOral, 3, pomada, fluconazol, mais18anos, null, 400mginiciale200depois, null, 1vezdia , [hipersensibilidadefluconazol]).
 
 % IMPORTANTE - APENAS SE A CAUSA FOR BACTERIANA CONSOANTE TESTE (Amigdalite em baixo)- PODEMOS ADICIONAR UM ANTIINFLAMATORIO
-medicamento(amigdalite, 2 , Comprimido , Amoxicilina , null , mais40kg, entre250mga1g, null, 2a3vezes , [hipersensibilidadeAntibioticosBetaLactamicos]).
-medicamento(amigdalite, 2e4 , Xarope , Amoxicilina , mais3anos, null, 5ml, 500mgpor5ml , 8em8horas, [hipersensibilidadeAntibioticosBetaLactamicos]).
-medicamento(amigdalite, 2e4 , Xarope , Amoxicilina , menos3anos, null, 5ml, 250mgpor5ml , 8em8horas, [hipersensibilidadeAntibioticosBetaLactamicos]).
-medicamento(amigdalite, 2e4 , Xarope , Amoxicilina, null, menos6kg, 2.5ml, 250mgpor5ml, 8em8horas , [hipersensibilidadeAntibioticosBetaLactamicos]).
-medicamento(otite, 2 , Comprimido , Amoxicilina , null , mais40kg, entre250mga1g, null, 2a3vezes , [hipersensibilidadeAntibioticosBetaLactamicos]).
-medicamento(otite, 2 , Xarope , Amoxicilina , mais3anos, null, 5ml, 500mgpor5ml , 8em8horas, [hipersensibilidadeAntibioticosBetaLactamicos]).
-medicamento(otite, 2 , Xarope , Amoxicilina , menos3anos, null, 5ml, 250mgpor5ml , 8em8horas, [hipersensibilidadeAntibioticosBetaLactamicos]).
-medicamento(otite, 2, Xarope , Amoxicilina, null, menos6kg, 2.5ml, 250mgpor5ml, 8em8horas , [hipersensibilidadeAntibioticosBetaLactamicos]).
-medicamento(otite, 5, Injecao, Cefuroxima, mais18anos, null, 750mg, null, 3vezesdia, [Gravidez, Amamentacao]).
-medicamento(otite, 5, Injecao, Cefuroxima, mais2meses, null, 30a100mg/kg/dia, null, 3a4vezesdia, [Gravidez, Amamentacao]).
-medicamento(otite, 5, Injecao, Cefuroxima, menos2meses, null, 30a100mg/kg/dia, null, 2a3vezesdia, [Gravidez, Amamentacao]).
-medicamento(zona, 6, Comprimido, Aciclovir, mais18anos, null, 800mg, null, 4a4horas, [AlergiaAciclovir, Gravidez, Amamentacao, Idosos, Criancas, Bebes]). % verificar com medico idosos criancas e bebes  
-medicamento(AlergiaAlimentar, 7, Comprimido, Cetirizina Aurobindo, mais12anos, null, 10mg,null, 1vezdia, null).
-medicamento(AlergiaAlimentar, 7, Xarope, Cetirizina Aurobindo, mais12anos, null, 5a10ml , 120ml, 1vezdia, null).
-medicamento(AlergiaAlimentar, 7, Xarope, Cetirizina Aurobindo, entre6e11anos, null, 5ml , 120ml, 12em12horas, null).
-medicamento(AlergiaAlimentar, 7, Xarope, Cetirizina Aurobindo, entre2e5anos, null, 2.5ml , 120ml, 12em12horas, null).
-medicamento(AlergiaRespiratoria, 7, Comprimido, Cetirizina Aurobindo, mais12anos, null, 10mg,null, 1vezdia, null).
-medicamento(AlergiaRespiratoria, 7, Xarope, Cetirizina Aurobindo, mais12anos, null, 5a10ml , 120ml, 1vezdia, null).
-medicamento(AlergiaRespiratoria, 7, Xarope, Cetirizina Aurobindo, entre6e11anos, null, 5ml , 120ml, 12em12horas, null).
-medicamento(AlergiaRespiratoria, 7, Xarope, Cetirizina Aurobindo, entre2e5anos, null, 2.5ml , 120ml, 12em12horas, null).
+medicamento(amigdalite, 2 , comprimido , amoxicilina , null , mais40kg, entre250mga1g, null, 2a3vezes , [hipersensibilidadeAntibioticosBetaLactamicos]).
+medicamento(amigdalite, 2e4 , xarope , amoxicilina , mais3anos, null, 5ml, 500mgpor5ml , 8em8horas, [hipersensibilidadeAntibioticosBetaLactamicos]).
+medicamento(amigdalite, 2e4 , xarope , amoxicilina , menos3anos, null, 5ml, 250mgpor5ml , 8em8horas, [hipersensibilidadeAntibioticosBetaLactamicos]).
+medicamento(amigdalite, 2e4 , xarope , amoxicilina, null, menos6kg, 2.5ml, 250mgpor5ml, 8em8horas , [hipersensibilidadeAntibioticosBetaLactamicos]).
+medicamento(otite, 2 , comprimido , amoxicilina , null , mais40kg, entre250mga1g, null, 2a3vezes , [hipersensibilidadeAntibioticosBetaLactamicos]).
+medicamento(otite, 2 , xarope , amoxicilina , mais3anos, null, 5ml, 500mgpor5ml , 8em8horas, [hipersensibilidadeAntibioticosBetaLactamicos]).
+medicamento(otite, 2 , xarope , amoxicilina , menos3anos, null, 5ml, 250mgpor5ml , 8em8horas, [hipersensibilidadeAntibioticosBetaLactamicos]).
+medicamento(otite, 2, xarope , amoxicilina, null, menos6kg, 2.5ml, 250mgpor5ml, 8em8horas , [hipersensibilidadeAntibioticosBetaLactamicos]).
+medicamento(otite, 5, injecao, cefuroxima, mais18anos, null, 750mg, null, 3vezesdia, [gravidez, amamentacao]).
+medicamento(otite, 5, injecao, cefuroxima, mais2meses, null, 30a100mg/kg/dia, null, 3a4vezesdia, [gravidez, amamentacao]).
+medicamento(otite, 5, injecao, cefuroxima, menos2meses, null, 30a100mg/kg/dia, null, 2a3vezesdia, [gravidez, amamentacao]).
+medicamento(zona, 6, comprimido, aciclovir, mais18anos, null, 800mg, null, 4a4horas, [alergiaAciclovir, gravidez, amamentacao, idosos, criancas, bebes]). % verificar com medico idosos criancas e bebes  
+medicamento(alergiaAlimentar, 7, comprimido, cetirizina_aurobindo, mais12anos, null, 10mg,null, 1vezdia, null).
+medicamento(alergiaAlimentar, 7, xarope, cetirizina_aurobindo, mais12anos, null, 5a10ml , 120ml, 1vezdia, null).
+medicamento(alergiaAlimentar, 7, xarope, cetirizina_aurobindo, entre6e11anos, null, 5ml , 120ml, 12em12horas, null).
+medicamento(alergiaAlimentar, 7, xarope, cetirizina_aurobindo, entre2e5anos, null, 2.5ml , 120ml, 12em12horas, null).
+medicamento(alergiaRespiratoria, 7, comprimido, cetirizina_aurobindo, mais12anos, null, 10mg,null, 1vezdia, null).
+medicamento(alergiaRespiratoria, 7, xarope, cetirizina_aurobindo, mais12anos, null, 5a10ml , 120ml, 1vezdia, null).
+medicamento(alergiaRespiratoria, 7, xarope, cetirizina_aurobindo, entre6e11anos, null, 5ml , 120ml, 12em12horas, null).
+medicamento(alergiaRespiratoria, 7, xarope, cetirizina_aurobindo, entre2e5anos, null, 2.5ml , 120ml, 12em12horas, null).
 
 
 
