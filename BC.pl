918e4aca250e61fac6554f100214f5801b101962
Destroar <126820622+Destroar@users.noreply.github.com>
2024-04-11 10:28:56 +0100

    Update BC.pl

--- BC.pl
+++ BC.pl
@@ -3,7 +3,7 @@
 :- op( 300, xfy, or).
 :- op( 200, xfy, and).
 
-if tosse_com_expetoracao and dificuldade_respiratoria and 
+if tosse_expetoracao and dificuldade_respiratoria and 
 (febre or arrepios_de_frio or dor_toracica or dor_cabeca or dor_muscular) then pneumonia.
 
 if placas_de_uma_substancia_cremosa_na_boca and aparecimento_de_aftas_na_lingua_ou_na_bochecha and 
@@ -11,7 +11,7 @@ if placas_de_uma_substancia_cremosa_na_boca and aparecimento_de_aftas_na_lingua_
 
 if dor_garganta and (febre or dificuldade_engolir or dor_de_ouvidos or mal_estar_geral) then amigdalite.
 
-if dor_ouvido and (dificuldade_em_dormir or choro_superior_ao_normal or dificuldades_de_audicao or febre
+if dor_ouvido and (dificuldade_em_dormir or choro_superior_ao_normal or dificuldades_audicao or febre
 or falta_apetite or dor_cabeca) then otite.
 
 if dor_cabeca and febre and (erupcoes_cutaneas or calafrios or nauseas or diarreia or dificuldade_em_urinar or dores_formigueiros_picadas_pele) then zona.
@@ -22,12 +22,12 @@ febre and calafrios or dor_lombar and nauseas and vomitos) then infeccao_urinari
 
 if coceira_na_boca and (coceira_na_pele or dor_abdominal or diarreia or enjoos or mal_estar_na_garganta or falta_ar or dificuldade_ao_respirar or rouquidao) then alergiaAlimentar.
 
-if nariz_entupido and espirros constantes and (coceira_nariz_olhos_garganta or nariz_vermelho or tosse_seca or dificuldade_respirar or olhos vermelhos or dor_cabeca)
+if nariz_entupido and espirros_constantes and (coceira_nariz_olhos_garganta or nariz_vermelho or tosse_seca or dificuldade_respirar or olhos_vermelhos or dor_cabeca)
 then alergiaRespiratoria.
 
-if tosse and falta_ar and (dor_peito or febre_baixa or fadiga) then bronquite.
+if (tosse_seca or tosse_expetoracao) and falta_ar and (dor_peito or febre or fadiga) then bronquite.
 
-if dor_cabeca and dor_um_lado_cabeca and mal_estar_geral and nauseas and (vomitos or tonturas or formigamento_corpo or sensibilidade_luz_ruidos) then enxaqueca.
+if dor_cabeca and sensibilidade_luz_ruidos and mal_estar_geral and nauseas and (vomitos or tonturas or formigamento_corpo) then enxaqueca.
 
 if febre and mal_estar_geral and dor_cabeca and (espirros or tosse_seca or nariz_entupido or dor_muscular or dor_garganta) then gripe.
 
@@ -36,5 +36,5 @@ if dor_articulacao and dificuldade_movimentar_articulacao and vermelhidao_articu
 if vermelhidao_olho and comichao and (laqueacao or sensacao_areia or sensacao_corpo_estranho or inchaco_palpebra or aumento_sensibilidade_luz or secrecao_purulenta) 
 then conjutivite_bacteriana.
 
-if congestao_nasal and corrimento_nasal and (pressao_facial or perda_olfato or febre or tosse or fadiga or mau_halito) then sinusite.
+if congestao_nasal and corrimento_nasal and (pressao_facial or perda_olfato or febre or tosse_expetoracao or tosse_seca or fadiga or mau_halito) then sinusite.
 
