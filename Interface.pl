626317384458420923b49696305e7a9d6955ed5c
MariaInesMaia <130787211+MariaInesMaia@users.noreply.github.com>
2024-04-14 00:18:33 +0100

    Interface

---
+++ Interface.pl
@@ -0,0 +1,23 @@
+menu:- write(' ----------------------------------------------------------------------------------------------------------'),nl,
+    write('|                               Seja Bem-Vindo Ao Teste De Sintomas                                        |'),nl,
+    write('|                                                                                                          |'),nl,
+    write('|----------------------------------------------------------------------------------------------------------|'),nl,
+    write('|Descubra possíveis doenças e tratamentos através deste teste.                                             |'),nl,
+    write('|                                                                                                          |'),nl,
+    write('|1-Iniciar o teste                                                                                         |'),nl,
+    write('|2-Sair                                                                                                    |'),nl,
+    read(X),
+    opcoes(X).
+
+opcoes(1):- questoesIniciais.
+opcoes(2):-
+    write('|Obrigada.                                                                                                 |'),halt.
+
+questoesIniciais:-
+    write('|----------------------------------------------------------------------------------------------------------|'),nl,
+    write('|Qual a sua idade?                                                                                         |'),nl,
+    read(I),
+    write('|                                                                                                          |'),nl,
+    write('|Qual o seu peso em kg?                                                                                          |'),nl,
+    read(P),
+    write('|                                                                                                          |').
