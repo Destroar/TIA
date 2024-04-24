928000f390e0857986b154ae88e079e56ba08ef7
MariaInesMaia <130787211+MariaInesMaia@users.noreply.github.com>
2024-04-24 15:07:32 +0100

    Update Interface.pl

--- Interface.pl
+++ Interface.pl
@@ -27,27 +27,19 @@ questoesIniciais:-
     write('|                                                                                                          |'),nl,
     write('|Identifique  grupo de idade a que pertence                                                                |'),nl,
     write('|                                                                                                          |'),nl,
-    write('|   1- [-3 meses]                          5- [5-9 anos]                                                   |'),nl,
-    write('|   2- [+3 meses]                          6- [10-13 anos]                                                 |'),nl,
-    write('|   3- [3-12 meses]                        7- [14-17 anos]                                                 |'),nl,
-    write('|   4- [1-4 anos]                          8- [+18 anos]                                                   |'),nl,
+    write('|   1- [-3 meses]                          5- [10-13 anos]                                                 |'),nl,
+    write('|   2- [3-12 meses]                        6- [14-17 anos]                                                 |'),nl,
+    write('|   3- [1-4 anos]                          7- [+18 anos]                                                   |'),nl,
+    write('|   4- [5-9 anos]                                                                                          |'),nl,
     read(I),
         ((I == 1), assert(fact(menos3meses)), confirmacaoPeso1;
-        (I == 2), assert(fact(mais3meses)), confirmacaoPeso1;
-        (I == 3), assert(fact(entre3a12meses)), confirmacaoPeso1;
-        (I == 4), assert(fact(entre1a4anos)), questoesSintomas;
-        (I == 5), assert(fact(entre5a9anos)), questoesSintomas;
-        (I == 6), assert(fact(entre10a13anos)), questoesSintomas;
-        (I == 7), assert(fact(entre14a17anos)), confirmacaoPeso2;
-        (I == 8), assert(fact(mais18anos)), confirmacaoPeso2).
-    %write('|----------------------------------------------------------------------------------------------------------|'),nl,
-    %write('|                                                                                                          |'),nl,
-    %write('|Nome: '),write(N),write('?'),nl,
-    %write('|Opção da idade: '),write(I),write('?'),nl,
-    %write('|                                                                                                          |'),nl,
-    %write('|                   Porfavor confirme os dados fornecidos para podermos iniciar o teste                    |'),nl,
-    %write('|----------------------------------------------------------------------------------------------------------|'),nl,confirmacao.
-
+        (I == 2), assert(fact(entre3a12meses)), confirmacaoPeso1;
+        (I == 3), assert(fact(entre1a4anos)), questoesSintomas;
+        (I == 4), assert(fact(entre5a9anos)), questoesSintomas;
+        (I == 5), assert(fact(entre10a13anos)), questoesSintomas;
+        (I == 6), assert(fact(entre14a17anos)), confirmacaoPeso2;
+        (I == 7), assert(fact(mais18anos)), confirmacaoPeso2).
+    
 confirmacaoPeso1:-
     nl, 
     write('|Confirma ter menos de 6kg                                                                                 |'),nl,
@@ -57,27 +49,15 @@ confirmacaoPeso1:-
         ((O6 == 1), assert(fact(menos6kg)), questoesSintomas;
         (O6 == 2), questoesSintomas).
 
-    confirmacaoPeso2:-
-        nl, 
-        write('|Confirma ter mis de 40kg                                                                                  |'),nl,
-        write('|1-Sim                                                                                                     |'),nl,
-        write('|2-Não                                                                                                     |'),nl,
-        read(O6),
-            ((O6 == 1), assert(fact(mais40kg)), questoesSintomas;
-            (O6 == 2), questoesSintomas).
+confirmacaoPeso2:-
+    nl, 
+    write('|Confirma ter mis de 40kg                                                                                  |'),nl,
+    write('|1-Sim                                                                                                     |'),nl,
+    write('|2-Não                                                                                                     |'),nl,
+    read(O6),
+        ((O6 == 1), assert(fact(mais40kg)), questoesSintomas;
+        (O6 == 2), questoesSintomas).
  
-%confirmacao:-
-    %write('|                                                                                                          |'),nl,
-    %write('|1-Cofirmado                                                                                               |'),nl,
-    %write('|2-Reintroduzir dados                                                                                      |'),nl,
-    %read(C),
-    %opcao(C).
-
-%opcao(1):- questoesSintomas.
-%opcao(2):- questoesIniciais.
-%opcao(_):-
-    %write('|Introduza uma opção válida.                                                                               |'),nl,confirmacao.
-
 questoesSintomas:-
     write('|----------------------------------------------------------------------------------------------------------|'),nl,
     write('|                                               SINTOMAS                                                   |'),nl,
@@ -91,10 +71,10 @@ questoesSintomas:-
     write('|Tem tosse seca?                                                                                           |'),nl,
     read(S),
     write('|                                                                                                          |'),nl,
-    write('|Tem tosse com expetoração?                                                                                           |'),nl,
+    write('|Tem tosse com expetoração?                                                                                |'),nl,
     read(E),
     write('|                                                                                                          |'),nl,
-    write('|Tem dificuldade a respirar?                                                                                           |'),nl,
+    write('|Tem dificuldade a respirar?                                                                               |'),nl,
     read(R),
     write('|                                                                                                          |'),nl,
     write('|Tem mal-estar geral?                                                                                      |'),nl,
@@ -118,19 +98,19 @@ questoesSintomas:-
     write('|Tem dor de garganta?                                                                                      |'),nl,
     read(G),
     write('|                                                                                                          |'),nl,
-    write('|Tem dor de ouvidos?                                                                                           |'),nl,
+    write('|Tem dor de ouvidos?                                                                                       |'),nl,
     read(O),
     write('|                                                                                                          |'),nl,
     write('|Tem dores musculares?                                                                                     |'),nl,
     read(S),
     write('|                                                                                                          |'),nl,
-    write('|Tem nariz entupido?                                                                                           |'),nl,
+    write('|Tem nariz entupido?                                                                                       |'),nl,
     read(Et),
     write('|                                                                                                          |'),nl,
-    write('|Tem falta de ar?                                                                                           |'),nl,
+    write('|Tem falta de ar?                                                                                          |'),nl,
     read(Ar),
     write('|                                                                                                          |'),nl,
-    write('|Tem vermelhidão nos olhos?                                                                                           |'),nl,
+    write('|Tem vermelhidão nos olhos?                                                                                |'),nl,
     read(S),
     write('|                                                                                                          |'),nl,
     write('|Tem tosse seca?                                                                                           |'),nl,
