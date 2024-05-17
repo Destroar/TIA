f5de59f6f0bef93ac6fefedf66cda1d9d8bea139
a100440 <160506851+a100440@users.noreply.github.com>
2024-05-17 21:15:36 +0100

    Alterações

--- Projeto 2/Parte A/sistema.pl
+++ Projeto 2/Parte A/sistema.pl
@@ -1,5 +1,7 @@
 :- include('tratamento.pl').
 
+arco(Origem,Destino) :- estado(Origem,Destino,_,_,_).
+
 caminho(X,Z,Caminho):- caminho(X,Z,[X],Y), reverse(Y,Caminho).
 caminho(X,X,Caminho,Caminho).
 caminho(X,Z,Visitado,Caminho):-
@@ -8,17 +10,34 @@ caminho(X,Z,Visitado,Caminho):-
     caminho(Y,Z,[Y | Visitado],Caminho).
 
 
+%calculo efetuado com o custo
+comprimento([],0,[]).
+comprimento([_],0,[]).
+comprimento([X,Y|R],N,LT):- estado(X,Y,C,_,T), comprimento([Y|R],N1,LT1), N is N1+C,append([T],LT1,LT).
 
-comprimento([],0).
-comprimento([_],0).
-comprimento([X,Y|R],N):- custo(X,Y,C), tempo(X,Y,T),
-    comprimento([Y|R],N1), N is N1+C*T.  %dieta é zero rever esta parte
+
+%calculo efetuado com o tempo
+comprimento1([],0).
+comprimento1([_],0).
+comprimento1([X,Y|R],N):- estado(X,Y,_,T,_), comprimento1([Y|R],N1), N is N1+T.
+
+%calculo efetuado com ambos
+comprimento2([],0).
+comprimento2([_],0).
+comprimento2([X,Y|R],N):- estado(X,Y,C,T,_), comprimento2([Y|R],N1), N is N1+C*T.
 
 
-menorcusto(X,Y,C):-caminho(X,Y,C), % gerar
+
+
+%Alterar o tipo de dados alterar zero por lista vazia
+%append([Tratamento],N1,N).
+
+
+
+menorCusto(X,Y,C):-caminho(X,Y,C), % gerar
 maiscurto(X,Y,C).% testar
-maiscurto(X,Y,C):- comprimento(C,NC),!,
+maiscurto(X,Y,C):- comprimento(C,NC,_),!,
 \+ menor(X,Y,NC).
 menor(X,Y,NC):- caminho(X,Y,C1),
-comprimento(C1,NC1),
+comprimento(C1,NC1,_),
 NC1<NC.
--- Projeto 2/Parte A/tratamento.pl
+++ Projeto 2/Parte A/tratamento.pl
@@ -1,185 +1,75 @@
-arco(estado_inicial, paracetamol).
-arco(estado_inicial, brufen).
-arco(estado_inicial, aspirina).
-arco(estado_inicial, anti_histaminico).
-arco(estado_inicial, descanso).
-arco(paracetamol, descanso).
-arco(paracetamol, fisioterapia).
-arco(paracetamol, cine_terapia).
-arco(paracetamol, antibiotico).
-arco(paracetamol, oculos).
-arco(paracetamol, lentes_contacto).
-arco(brufen, descanso).
-arco(brufen, desporto).
-arco(brufen, fisioterapia).
-arco(brufen, ginastica).
-arco(brufen, oculos).
-arco(brufen, lentes_contacto).
-arco(aspirina, inalacoes).
-arco(aspirina, insulina).
-arco(aspirina, antibiotico).
-arco(aspirina, dieta).
-arco(aspirina, soro).
-arco(aspirina, oculos).
-arco(aspirina, lentes_contacto).
-arco(anti_histaminico, antibiotico).
-arco(anti_histaminico, internamento).
-arco(descanso, cine_terapia).
-arco(descanso, oculos).
-arco(descanso, lentes_contacto).
-arco(descanso, ambulatorio).
-arco(fisioterapia, cirurgia).
-arco(fisioterapia, ginastica).
-arco(fisioterapia, calmante).
-arco(fisioterapia, injecoes).
-arco(fisioterapia, ambulatorio).
-arco(fisioterapia,estado_final).
-arco(cine_terapia, calmante).
-arco(cine_terapia, estado_final).
-arco(desporto, dieta).
-arco(desporto, calmante).
-arco(cirurgia, internamento).
-arco(cirurgia, soro).
-arco(inalacoes, soro).
-arco(insulina, dialise).
-arco(insulina, injecoes).
-arco(antibiotico, internamento).
-arco(antibiotico, estado_final).
-arco(dieta, estado_final).
-arco(ginastica, ambulatorio).
-arco(internamento, estado_final).
-arco(dialise, estado_final).
-arco(calmante, ambulatorio).
-arco(injecoes, inalo_terapia).
-arco(injecoes, ambulatorio).
-arco(inalo_terapia, estado_final).
-arco(soro, ambulatorio).
-arco(oculos, estado_final).
-arco(lentes_contacto, estado_final).
-arco(ambulatorio, estado_final).
-
-
-custo(estado_inicial, paracetamol, 5).
-custo(estado_inicial, brufen, 4).
-custo(estado_inicial, aspirina, 2).
-custo(estado_inicial, anti_histaminico, 8).
-custo(estado_inicial, descanso, 100).
-custo(paracetamol, descanso, 100).
-custo(paracetamol, fisioterapia, 150).
-custo(paracetamol, cine_terapia, 200).
-custo(paracetamol, antibiotico, 25).
-custo(paracetamol, oculos, 500).
-custo(paracetamol, lentes_contacto, 300).
-custo(brufen, descanso, 100).
-custo(brufen, desporto, 0).
-custo(brufen, fisioterapia, 150).
-custo(brufen, ginastica, 90).
-custo(brufen, oculos, 500).
-custo(brufen, lentes_contacto, 300).
-custo(aspirina, inalacoes, 50).
-custo(aspirina, insulina, 34).
-custo(aspirina, antibiotico, 25).
-custo(aspirina, dieta, 0).
-custo(aspirina, soro, 15).
-custo(aspirina, oculos, 500).
-custo(aspirina, lentes_contacto, 300).
-custo(anti_histaminico, antibiotico, 25).
-custo(anti_histaminico, internamento, 250).
-custo(descanso, cine_terapia, 200).
-custo(descanso, oculos, 500).
-custo(descanso, lentes_contacto, 300).
-custo(descanso, ambulatorio, 800).
-custo(fisioterapia, cirurgia, 1200).
-custo(fisioterapia, ginastica, 90).
-custo(fisioterapia, calmante, 80).
-custo(fisioterapia, injecoes, 75).
-custo(fisioterapia, ambulatorio, 800).
-custo(fisioterapia,estado_final,0).
-custo(cine_terapia, calmante, 80).
-custo(cine_terapia, estado_final, 0).
-custo(desporto, dieta, 0).
-custo(desporto, calmante, 80).
-custo(cirurgia, internamento, 250).
-custo(cirurgia, soro, 15).
-custo(inalacoes, soro, 15).
-custo(insulina, dialise, 100).
-custo(insulina, injecoes, 75).
-custo(antibiotico, internamento, 250).
-custo(antibiotico, estado_final, 0).
-custo(dieta, estado_final, 0).
-custo(ginastica, ambulatorio, 800).
-custo(internamento, estado_final, 0).
-custo(dialise, estado_final, 0).
-custo(calmante, ambulatorio, 800).
-custo(injecoes, inalo_terapia, 90).
-custo(injecoes, ambulatorio, 800).
-custo(inalo_terapia, estado_final, 0).
-custo(soro, ambulatorio, 800).
-custo(oculos, estado_final, 0).
-custo(lentes_contacto, estado_final, 0).
-custo(ambulatorio, estado_final, 0).
+estado(estado_clinico_inicial, 2,5,2,'Paracetamol').
+estado(estado_clinico_inicial, 3,4,3,'Brufen').
+estado(estado_clinico_inicial, 4,2,4,'Aspirina').
+estado(estado_clinico_inicial, 5,8,5,'Anti-histaminico').
+estado(estado_clinico_inicial, 6,100,2,'Descanso').
+estado(2,6,100,2,'Descanso').
+estado(3,6,100,2,'Descanso').
+estado(2,7,150,6,'Fisioterapia').
+estado(2,estado_clinico_final,150,6,'Fisioterapia').
+estado(3,7,150,6,'Fisioterapia').
+estado(3,estado_clinico_final,150,6,'Fisioterapia').
+estado(2,8,200,8,'Cine terapia').
+estado(6,8,200,8,'Cine terapia').
+estado(6,estado_clinico_final,200,8,'Cine terapia').
+estado(3,9,0,12,'Desporto').
+estado(7,10,1200,3,'Cirugia').
+estado(4,11,50,10,'Inalacoes').
+estado(4,12,34,30,'Insulina').
+estado(2,13,25,8,'Antibiotico').
+estado(4,13,25,8,'Antibiotico').
+estado(4,estado_clinico_final,25,8,'Antibiotico').
+estado(5,13,25,8,'Antibiotico').
+estado(5,estado_clinico_final,25,8,'Antibiotico').
+estado(4,estado_clinico_final,0,30,'Dieta').
+estado(9,estado_clinico_final,0,30,'Dieta').
+estado(3,15,90,15,'Ginastica').
+estado(7,15,90,15,'Ginastica').
+estado(5,estado_clinico_final,250,3,'Internamento').
+estado(10,estado_clinico_final,250,3,'Internamento').
+estado(13,estado_clinico_final,250,3,'Internamento').
+estado(12,estado_clinico_final,100,10,'Dialise').
+estado(7,17,80,90,'Calmante').
+estado(8,17,80,90,'Calmante').
+estado(9,17,80,90,'Calmante').
+estado(7,18,75,3,'Injecoes').
+estado(12,18,75,3,'Injecoes').
+estado(18,estado_clinico_final,90,6,'Inalo-terapia').
+estado(4,20,15,8,'Soro').
+estado(10,20,15,8,'Soro').
+estado(11,20,15,8,'Soro').
+estado(16,20,15,8,'Soro').
+%estado(2,estado_clinico_final,500,1000,'Oculos').
+estado(3,estado_clinico_final,500,1000,'Oculos').
+estado(4,estado_clinico_final,500,1000,'Oculos').
+estado(6,estado_clinico_final,500,1000,'Oculos').
+%estado(2,estado_clinico_final,300,200,'Lentes contacto').
+estado(3,estado_clinico_final,300,200,'Lentes contacto').
+estado(4,estado_clinico_final,300,200,'Lentes contacto').
+estado(6,estado_clinico_final,300,200,'Lentes contacto').
+estado(6,estado_clinico_final,800,20,'Ambulatorio').
+estado(15,estado_clinico_final,800,20,'Ambulatorio').
+estado(7,estado_clinico_final,800,20,'Ambulatorio').
+estado(17,estado_clinico_final,800,20,'Ambulatorio').
+estado(18,estado_clinico_final,800,20,'Ambulatorio').
+estado(20,estado_clinico_final,800,20,'Ambulatorio').
 
 
 
-tempo(estado_inicial, paracetamol, 2).
-tempo(estado_inicial, brufen, 3).
-tempo(estado_inicial, aspirina, 4).
-tempo(estado_inicial, anti_histaminico, 5).
-tempo(estado_inicial, descanso, 2).
-tempo(paracetamol, descanso, 2).
-tempo(paracetamol, fisioterapia, 6).
-tempo(paracetamol, cine_terapia, 8).
-tempo(paracetamol, antibiotico, 8).
-tempo(paracetamol, oculos, 1000).
-tempo(paracetamol, lentes_contacto, 200).
-tempo(brufen, descanso, 2).
-tempo(brufen, desporto, 12).
-tempo(brufen, fisioterapia, 6).
-tempo(brufen, ginastica, 15).
-tempo(brufen, oculos, 1000).
-tempo(brufen, lentes_contacto, 200).
-tempo(aspirina, inalacoes, 10).
-tempo(aspirina, insulina, 30).
-tempo(aspirina, antibiotico, 8).
-tempo(aspirina, dieta, 30).
-tempo(aspirina, soro, 8).
-tempo(aspirina, oculos, 1000).
-tempo(aspirina, lentes_contacto, 200).
-tempo(anti_histaminico, antibiotico, 8).
-tempo(anti_histaminico, internamento, 3).
-tempo(descanso, cine_terapia, 8).
-tempo(descanso, oculos, 1000).
-tempo(descanso, lentes_contacto, 200).
-tempo(descanso, ambulatorio, 20).
-tempo(fisioterapia, cirurgia, 3).
-tempo(fisioterapia, ginastica, 15).
-tempo(fisioterapia, calmante, 90).
-tempo(fisioterapia, injecoes, 3).
-tempo(fisioterapia, ambulatorio, 20).
-tempo(fisioterapia,estado_final,0).
-tempo(cine_terapia, calmante, 90).
-tempo(cine_terapia, estado_final, 0).
-tempo(desporto, dieta, 30).
-tempo(desporto, calmante, 90).
-tempo(cirurgia, internamento, 3).
-tempo(cirurgia, soro, 8).
-tempo(inalacoes, soro, 8).
-tempo(insulina, dialise, 10).
-tempo(insulina, injecoes, 3).
-tempo(antibiotico, internamento, 3).
-tempo(antibiotico, estado_final, 0).
-tempo(dieta, estado_final, 0).
-tempo(ginastica, ambulatorio, 20).
-tempo(internamento, estado_final, 0).
-tempo(dialise, estado_final, 0).
-tempo(calmante, ambulatorio, 20).
-tempo(injecoes, inalo_terapia, 6).
-tempo(injecoes, ambulatorio, 20).
-tempo(inalo_terapia, estado_final, 0).
-tempo(soro, ambulatorio, 20).
-tempo(oculos, estado_final, 0).
-tempo(lentes_contacto, estado_final, 0).
-tempo(ambulatorio, estado_final, 0).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
