bfbc381418232c6a81df4ba641fff853c54a5523
Catarinaeeee <161334518+CatarinaBFeee@users.noreply.github.com>
2024-05-21 14:12:59 +0100

    Interface

---
+++ Projeto 2/Parte A/interface.pl
@@ -0,0 +1,58 @@
+:- op( 800, fx, if).
+:- op( 700, xfx, then).
+:- op( 300, xfy, or).
+:- op( 200, xfy, and).
+
+
+:- include('sistema.pl').
+:- include('tratamento.pl').
+# :- include('BI.pl').
+
+
+menu:-
+    write(' ----------------------------------------------------------------------------------------------------------'),nl,
+    write('|                               Seja Bem-Vindo Aos Tratamentos Possiveis                                   |'),nl,
+    write('|                                                                                                          |'),nl,
+    write('|----------------------------------------------------------------------------------------------------------|'),nl,
+    write('|                                                                                                          |'),nl,
+    menu2(XI),menu3(XF),metodo(M),objetivo(O).
+
+menu2(XI):-
+    write('|                                                                                                          |'),nl,
+    write('|                                     Indique o estado incial?                                             |'),nl,
+    write('|Indique apenas numeros.                                                                                   |'),nl,
+    write('|----------------------------------------------------------------------------------------------------------|'),nl,
+    read(XI).
+
+
+    menu3(XF):-
+    write('|                                                                                                          |'),nl,
+    write('|                        Qual o destino que quer chegar?                                                   |'),nl,
+    write('|Indique apenas numeros.                                                                                   |'),nl,
+    write('|----------------------------------------------------------------------------------------------------------|'),nl,
+    write('|                                                                                                          |'),nl,
+    read(XF).
+ 
+
+metodo(M):-
+    write('|----------------------------------------------------------------------------------------------------------|'),nl,
+    write('|                                        Indique o metodo que quer utilizar                                |'),nl,
+    write('|                                                                                                          |'),nl,
+    write('|1 - Hill Climbing                                                                                         |'),nl,
+    write('|2 - Caminho em Grafos                                                                                     |'),nl,
+    write('|2 - Depthfirst                                                                                            |'),nl,
+    write('|3 - Breadthfirst                                                                                          |'),nl,
+    write('|                                                                                                          |'),nl,
+    read(M).
+
+
+objetivo(O):-
+    write('|----------------------------------------------------------------------------------------------------------|'),nl,
+    write('|                                        Indique o objetivo                                                |'),nl,
+    write('|                                                                                                          |'),nl,
+    write('|1 - Menor tempo possivel                                                                                  |'),nl,
+    write('|2 - Menor custo possivel                                                                                  |'),nl,
+    write('|3 - Ambos os anteriores                                                                                   |'),nl,
+    write('|                                                                                                          |'),nl,
+    read(O).
+
