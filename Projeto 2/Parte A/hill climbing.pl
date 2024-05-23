dd1f7ecee610ab1911a53d10e7e83f151a6cb9e3
MariaInesMaia <130787211+MariaInesMaia@users.noreply.github.com>
2024-05-23 13:16:04 +0100

    Hill climbing
    tentativa

---
+++ Projeto 2/Parte A/hill climbing.pl
@@ -0,0 +1,39 @@
+% hill climbing
+% Prob=0 is pure hill climbing, Prob>0 means Stochastic Hill Climbing
+% S0 is the initial solution, Control is a list with the number of iterations, verbose in console, probability and type of optimization.
+hill_climbing(S0,[Iter,Verbose,Prob,Opt],S1):-
+	obj(O),
+	((O == 1), comprimento(S0, E0, _);
+    (O == 2), comprimento1(S0, E0, _) ;
+    (O == 3), comprimento2(S0, E0, _)),
+	hill_climbing(S0,E0,0,Iter,Verbose,Prob,Opt,S1).
+
+hill_climbing(S, _, Iter, Iter, S):- !.
+hill_climbing(S1,E1,I,Iter,Verbose,Prob,Opt,SFinal):-
+    I < Iter,
+    write("teste"),    
+	change(S1,SNew),
+	best(Prob,Opt,S1,E1,SNew,_,S2,E2),
+	I1 is I+1,
+	hill_climbing(S2,E2,I1,Iter,Verbose,Prob,Opt,SFinal).
+
+% return SR, the best value of S1 and S2: SR (solution) and ER (eval)
+best(Prob,Opt,S1,E1,S2,E2,SR,ER):-
+	obj(O),
+	((O == 1), comprimento(S2, E2, _);
+    (O == 2), comprimento1(S2, E2, _) ;
+    (O == 3), comprimento2(S2, E2, _)),
+	best_opt(Prob,Opt,S1,E1,S2,E2,SR,ER).
+
+best_opt(Prob,_,_,_,S2,E2,S2,E2):-
+	random(X), % random from 0 to 1,
+	X< Prob. % accept new solution
+
+best_opt(_,Opt,S1,E1,S2,E2,SR,ER):- % else, select the best one
+    ( (Opt=max,max_list([E1,E2],ER));(Opt=min,min_list([E1,E2],ER)) ),
+    ( (ER==E1,SR=S1); (ER==E2,SR=S2) ).
+
+change(S, SNEW):-origem(O), destino(D),
+    findall(X,caminho(O,D,X),L), length(L,LE), random(0,LE,R), nth0(R,L,SNEW),
+    dif(SNEW,S).
+change(S,SNEW):-change(S,SNEW).
--- Projeto 2/Parte A/interface.pl
+++ Projeto 2/Parte A/interface.pl
@@ -1,6 +1,8 @@
 
 :- include('sistema.pl').
 :- include('tratamento.pl').
+:- include('hill climbing.pl').
+:- use_module(library(lists)).
 
 menu:-
     write(' ----------------------------------------------------------------------------------------------------------'),nl,
@@ -8,14 +10,16 @@ menu:-
     write('|                                                                                                          |'),nl,
     write('|----------------------------------------------------------------------------------------------------------|'),nl,
     write('|                                                                                                          |'),nl,
-    menu2(XI),menu3(XF),metodo(M),objetivo(O),
-    ((O == 1), menortempo(XI, XF, Caminho);
-    (O == 2), menorcusto(XI, XF, Caminho) ;
-    (O == 3), menordois(XI, XF, Caminho)),
+    menu2(XI),menu3(XF),metodo(M), objetivo(O), write("teste"), assert(obj(O)), write("teste"),
+    ((M == 1), caminho(XI, XF, C), assert(origem(XI)), assert(destino(XF)), write("teste"), hill_climbing(C,[20,0,0,min],Caminho)),
+    ((M == 2), ((O == 1), menorcusto(XI, XF, Caminho);
+    (O == 2), menortempo(XI, XF, Caminho) ;
+    (O == 3), menordois(XI, XF, Caminho))),
     comprimento(Caminho, _, Tratamento),
     write("Caminho: "), write(Caminho), nl,
-    write("Tratamento: "), write(Tratamento).
-
+    write("Tratamento: "), write(Tratamento),
+    retract_all_asserts.
+    
 menu2(XI):-
     write('|                                                                                                          |'),nl,
     write('|                                     Indique o estado incial?                                             |'),nl,
@@ -49,9 +53,17 @@ objetivo(O):-
     write('|----------------------------------------------------------------------------------------------------------|'),nl,
     write('|                                        Indique o objetivo                                                |'),nl,
     write('|                                                                                                          |'),nl,
-    write('|1 - Menor tempo possivel                                                                                  |'),nl,
-    write('|2 - Menor custo possivel                                                                                  |'),nl,
+    write('|1 - Menor custo possivel                                                                                  |'),nl,
+    write('|2 - Menor tempo possivel                                                                                  |'),nl,
     write('|3 - Ambos os anteriores                                                                                   |'),nl,
     write('|                                                                                                          |'),nl,
     read(O).
 
+    retract_all_asserts :-
+        retract(obj(_)),
+        retract(origem(_)),
+        retract(destino(_)).
+        
+        
+        
+      
